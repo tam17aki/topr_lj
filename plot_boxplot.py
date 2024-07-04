e2a16394154605fb9c7e63c156e9cce32e2d0985
Akira Tamamori <tamamori5917@gmail.com>
2024-07-04 22:10:45 +0900

    add new function.

--- plot_boxplot.py
+++ plot_boxplot.py
@@ -26,54 +26,62 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
-from hydra import compose, initialize
-from omegaconf import DictConfig
+import numpy.typing as npt
+
+from config import ModelConfig, PathConfig
 
 
-def main(cfg: DictConfig):
-    """Plot boxplot of scores."""
-    score_dir = os.path.join(cfg.TOPR.root_dir, cfg.TOPR.score_dir)
-    if cfg.model.n_lookahead == 0:
+def load_scores(mode: str, score_dir: str) -> dict[str, npt.NDArray[np.float]]:
+    """Load objective scores.
+
+    Args:
+        mode (str): mode.
+        score_dir (str): score directory.
+    """
+    model_cfg = ModelConfig()
+    if model_cfg.n_lookahead == 0:
         score_file = {"SPSI": "", "RTISI": "", "RTPGHI": "", "TOPR": ""}
     else:
         score_file = {"RTISI": "", "PGHI": "", "TOPR": ""}
-    fig_dir = os.path.join(cfg.TOPR.root_dir, "fig")
+    score = {}
+    if model_cfg.n_lookahead == 0:
+        score_file["SPSI"] = os.path.join(score_dir, f"{mode}_score_SPSI.txt")
+        score_file["RTISI"] = os.path.join(score_dir, f"{mode}_score_RTISI.txt")
+        score_file["RTPGHI"] = os.path.join(score_dir, f"{mode}_score_RTPGHI.txt")
+        score_file["TOPR"] = os.path.join(score_dir, f"{mode}_score_TOPR_online.txt")
+        with open(score_file["SPSI"], mode="r", encoding="utf-8") as file_hander:
+            score["SPSI"] = np.array([float(line.strip()) for line in file_hander])
+        with open(score_file["RTISI"], mode="r", encoding="utf-8") as file_hander:
+            score["RTISI"] = np.array([float(line.strip()) for line in file_hander])
+        with open(score_file["RTPGHI"], mode="r", encoding="utf-8") as file_hander:
+            score["RTPGHI"] = np.array([float(line.strip()) for line in file_hander])
+    else:
+        score_file["RTISI"] = os.path.join(score_dir, f"{mode}_score_RTISI_LA.txt")
+        score_file["PGHI"] = os.path.join(score_dir, f"{mode}_score_PGHI.txt")
+        score_file["TOPR"] = os.path.join(score_dir, f"{mode}_score_TOPR_offline.txt")
+        with open(score_file["RTISI"], mode="r", encoding="utf-8") as file_hander:
+            score["RTISI"] = np.array([float(line.strip()) for line in file_hander])
+        with open(score_file["PGHI"], mode="r", encoding="utf-8") as file_hander:
+            score["PGHI"] = np.array([float(line.strip()) for line in file_hander])
+
+    with open(score_file["TOPR"], mode="r", encoding="utf-8") as file_hander:
+        score["TOPR"] = np.array([float(line.strip()) for line in file_hander])
+
+    return score
+
+
+def main():
+    """Plot boxplot of scores."""
+    path_cfg = PathConfig()
+    model_cfg = ModelConfig()
+    score_dir = os.path.join(path_cfg.root_dir, "score")
+    fig_dir = os.path.join(path_cfg.root_dir, "fig")
     os.makedirs(fig_dir, exist_ok=True)
     fig = plt.figure(figsize=(12, 4))
     for i, mode in enumerate(("stoi", "pesq", "lsc")):
-        score = {}
-        if cfg.model.n_lookahead == 0:
-            score_file["SPSI"] = os.path.join(score_dir, f"{mode}_score_SPSI.txt")
-            score_file["RTISI"] = os.path.join(score_dir, f"{mode}_score_RTISI.txt")
-            score_file["RTPGHI"] = os.path.join(score_dir, f"{mode}_score_RTPGHI.txt")
-            score_file["TOPR"] = os.path.join(
-                score_dir, f"{mode}_score_TOPR_online.txt"
-            )
-            with open(score_file["SPSI"], mode="r", encoding="utf-8") as file_hander:
-                score["SPSI"] = np.array([float(line.strip()) for line in file_hander])
-            with open(score_file["RTISI"], mode="r", encoding="utf-8") as file_hander:
-                score["RTISI"] = np.array([float(line.strip()) for line in file_hander])
-            with open(score_file["RTPGHI"], mode="r", encoding="utf-8") as file_hander:
-                score["RTPGHI"] = np.array(
-                    [float(line.strip()) for line in file_hander]
-                )
-        else:
-            score_file["RTISI"] = os.path.join(score_dir, f"{mode}_score_RTISI_LA.txt")
-            score_file["PGHI"] = os.path.join(score_dir, f"{mode}_score_PGHI.txt")
-            score_file["TOPR"] = os.path.join(
-                score_dir, f"{mode}_score_TOPR_offline.txt"
-            )
-            with open(score_file["RTISI"], mode="r", encoding="utf-8") as file_hander:
-                score["RTISI"] = np.array([float(line.strip()) for line in file_hander])
-            with open(score_file["PGHI"], mode="r", encoding="utf-8") as file_hander:
-                score["PGHI"] = np.array([float(line.strip()) for line in file_hander])
-
-        with open(score_file["TOPR"], mode="r", encoding="utf-8") as file_hander:
-            score["TOPR"] = np.array([float(line.strip()) for line in file_hander])
-
+        score = load_scores(mode, score_dir)
         axes = fig.add_subplot(1, 3, i + 1)
-
-        if cfg.model.n_lookahead == 0:
+        if model_cfg.n_lookahead == 0:
             axes.boxplot(
                 np.concatenate(
                     (
@@ -84,7 +92,7 @@ def main(cfg: DictConfig):
                     ),
                     axis=1,
                 ),
-                flierprops=dict(marker="+", markeredgecolor="r"),
+                flierprops={"marker": "+", "markeredgecolor": "r"},
                 labels=["SPSI", "RTISI", "RTPGHI", "TOPR"],
                 widths=(0.5, 0.5, 0.5, 0.5),
             )
@@ -98,7 +106,7 @@ def main(cfg: DictConfig):
                     ),
                     axis=1,
                 ),
-                flierprops=dict(marker="+", markeredgecolor="r"),
+                flierprops={"marker": "+", "markeredgecolor": "r"},
                 labels=["RTISI", "PGHI", "TOPR"],
                 widths=(0.5, 0.5, 0.5),
             )
@@ -114,7 +122,7 @@ def main(cfg: DictConfig):
         else:
             axes.set_title(mode.upper(), fontsize=16)
     fig.tight_layout()
-    if cfg.model.n_lookahead == 0:
+    if model_cfg.n_lookahead == 0:
         plt.savefig(os.path.join(fig_dir, "score_online.png"))
     else:
         plt.savefig(os.path.join(fig_dir, "score_offline.png"))
@@ -122,6 +130,4 @@ def main(cfg: DictConfig):
 
 
 if __name__ == "__main__":
-    with initialize(version_base=None, config_path="."):
-        config = compose(config_name="config")
-    main(config)
+    main()
